5587f732ad5088e165b46bd2b1da3f0895568f09
Abdurahmon-Dev <abdurahmonmirobidov2004@gmail.com>
2021-07-15 15:05:36 +0500

    add timer

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
-const numbers = [1,2,3,4,5];
-const [y] = numbers;
-console.log(y)
+let that_day = new Date('Nov 1 2021 00:00:00');
+   
+function fixDate() {
+    let now = new Date();
+    let gap = that_day - now;
+    
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+    let minute = Math.floor(gap / 1000 / 60) % 60;
+    let secunde = Math.floor(gap / 1000) % 60;
+
+    getElement(days,hours,minute,secunde);
+}
+function getElement(day,hour,minute,secund) {
+    let TextDays = document.getElementById('day');
+    TextDays.innerText = day;
+
+    let TextHour = document.getElementById('hour');
+    TextHour.innerText = hour;
+
+    let TextMinute = document.getElementById('minute');
+    TextMinute.innerText = minute;
+
+    let TextSecunde = document.getElementById('secund');
+    TextSecunde.innerText = secund;
+}
+
+window.addEventListener('load', () => {
+    setInterval(fixDate, 1000);
+});
+
+
